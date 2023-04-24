b327e1505105ef3c4dfab213b213845e5a19d363
Ruslan <zzzlyka221@gmail.com>
2023-04-25 00:03:01 +0500

    #10 - в фильтры добавлен choicefield вместо charfield

--- core/filters.py
+++ core/filters.py
@@ -1,6 +1,7 @@
 import django_filters
 from django_filters import FilterSet
 from .models import *
+from .consts import STATUS_CHOICES
 
 
 class ApplicantFilter(FilterSet):
@@ -20,7 +21,7 @@ class AppealFilter(FilterSet):
     first_name = django_filters.CharFilter(field_name='applicant__first_name', lookup_expr='icontains')
     last_name = django_filters.CharFilter(field_name='applicant__last_name', lookup_expr='icontains')
     service_code = django_filters.CharFilter(field_name='emergency_services__service_code', lookup_expr='exact')
-    status = django_filters.CharFilter(field_name='status', lookup_expr='exact')
+    status = django_filters.ChoiceFilter(field_name='status', choices=STATUS_CHOICES)
 
     class Meta:
         model = Appeal
